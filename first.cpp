94f7ff3fde0278541ec4bd3db3aca32ab57b6fb2
HarshSingh21 <singhharsh8283@gmail.com>
2022-08-29 22:49:54 +0530

    first program

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+/*
+have
+to start with 
+for mutile __LINE__
+comment 
+we */
+// and when to comment single line use//
+{
+   cout<<"hi harsh";
+
+} 
+
