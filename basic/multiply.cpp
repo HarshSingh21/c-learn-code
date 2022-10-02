00088307c329e6983f5f80072e3e3022fd9031f7
HarshSingh21 <singhharsh8283@gmail.com>
2022-10-02 20:31:50 +0530

    table of any number

---
+++ basic/multiply.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+//display program for 1 to n from loop
+using namespace std;
+int main()
+{
+    int i,n,q;
+    cout<<"enter a number ";
+    cin>>n;
+    for (i=1;i<=10;i++)
+    {
+        cout<<n<<" X "<<i<<" = "<<i*n<<endl;
+    }
+    return 0;
+}
