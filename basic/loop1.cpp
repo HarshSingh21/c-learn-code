8ea5a173186bf8ff28d6c0af3dd2f1a14a9c621e
HarshSingh21 <singhharsh8283@gmail.com>
2022-10-02 20:32:35 +0530

    mutily

---
+++ basic/loop1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+//display program for 1 to n from loop
+using namespace std;
+
+//display program for 1 to n from loop
+// from while loop 
+// int main()
+// {    int n , i=1;
+// cout<<"enter a number";
+// cin>>n;
+// while (i<=n)
+// {
+//     cout<<i<<endl;
+//     i++;
+// }
+//     return 0;
+    
+// }
+
+
+
+// do while loop
+int main()
+{    int n , i=1;
+cout<<"enter a number";
+cin>>n;
+do {
+    cout<<i<<endl;
+    i++;
+}
+while (i<=n);
+    return 0;
+    
+}
