8db9529cc3e9e2fb730c101f234b1ab1c009c024
HarshSingh21 <singhharsh8283@gmail.com>
2022-10-03 00:09:58 +0530

    rverse_nloop

---
+++ basic/reversenloop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    for(i=n;i>0;i--)
+    {
+        cout<<i<<'\n';
+    }
+    return 0;
+}
