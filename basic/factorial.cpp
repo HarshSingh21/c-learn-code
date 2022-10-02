db2b2b66af7b9e102b8ae5c0bc5e6d0516de8c9a
HarshSingh21 <singhharsh8283@gmail.com>
2022-10-02 22:48:03 +0530

    factorial

---
+++ basic/factorial.cpp
@@ -0,0 +1,15 @@
+//factorail of n number 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,n,fact=1;
+    cout<<"Enter a Number ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        fact *=i;
+    }
+    cout<<"Factorial is = "<<fact;
+    return 0;
+}
