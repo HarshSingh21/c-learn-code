22c60231a5999ce3c61c5c9cdee2977d3ad0af55
HarshSingh21 <singhharsh8283@gmail.com>
2022-10-14 19:45:15 +0530

    pattern2

---
+++ Pattern/pattern2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<" pattern reverse   ";
+    int n;
+    cin>>n;
+    for(int i=n;i>=1;i--)
+ {
+    for(int j=i;j>0;j--)
+    {
+        cout<<"*";
+
+    }
+    cout<<endl;
+ }
+ 
+    return 0;
+}
