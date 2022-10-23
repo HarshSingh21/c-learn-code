9104aee98d0ea3ad8d7ee8ae79049842721dc013
HarshSingh21 <singhharsh8283@gmail.com>
2022-10-23 23:45:30 +0530

    1-9

---
+++ Pattern/1-9.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+    
+int main()
+{
+	for(int i=0;i<5;i++)
+	{
+	    for(int j=0;j<5;j++)
+	    {
+			if(i>=j)    //think about matrix i and j
+		    cout<<"*";
+	    }
+	        
+	    cout<<endl;
+	}
+	    
+	return 0;
+	
+}
+ 
