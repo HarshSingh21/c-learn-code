1fafbe3305691d196448e04701964c532171d2a3
Harsh Singh <94156692+HarshSingh21@users.noreply.github.com>
2022-10-25 23:02:30 +0530

    Update array_sum.cpp

--- Array/array_sum.cpp
+++ Array/array_sum.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {   int sum=0;
     cout<<"sum  of an array ";
-    int S[]={2,2,2,2,2,2,2};
+    int S[]={2,2,2,2,2,2,2,2};
     for(int i=0;i<=6;i++)
       sum = sum+S[i];        
       cout<<sum;
