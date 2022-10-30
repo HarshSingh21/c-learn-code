2147b919d1313d7c35078536d86f059db7315946
Harsh Singh <94156692+HarshSingh21@users.noreply.github.com>
2022-10-30 10:21:12 +0530

    Update dess.cpp

--- basic/dess.cpp
+++ basic/dess.cpp
@@ -14,7 +14,7 @@ void pop() {
 if(top<=-1)
 cout<<"Stack Underflow"<<endl;
 else {
-cout<<"The popped element is "<< stack[top] <<endl;
+cout<<"The popped element  is "<< stack[top] <<endl;
 top--;
 }
 }
