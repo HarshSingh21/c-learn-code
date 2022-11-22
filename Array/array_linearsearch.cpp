c2d0c8996da68eed7ab519df7e33ca1a0fbc3d81
Harsh Singh <94156692+HarshSingh21@users.noreply.github.com>
2022-11-22 21:27:08 +0530

    Update array_linearsearch.cpp

--- Array/array_linearsearch.cpp
+++ Array/array_linearsearch.cpp
@@ -4,15 +4,13 @@ int main()
 {
  	int A[10],n=10;
 	int key;
-	cout<<"Enter Numbers";
-	    
+	cout<<"Enter Numbers";    
 	for(int i=0;i<n;i++)
 	{
 		cin>>A[i];
 	}
 	cout<<"Enter Key";
 	cin>>key;
-	    
 	for(int i=0;i<n;i++)
 	{
 		if(key==A[i])
