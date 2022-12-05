d42606373827a651d22455c020c2c02c6b992811
Harsh Singh <94156692+HarshSingh21@users.noreply.github.com>
2022-12-05 20:10:35 +0530

    Update array_userinput.cpp

--- Array/array_userinput.cpp
+++ Array/array_userinput.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main()
 {
   int numbers[5];
+  
   cout << "Enter 5 numbers: " << endl;
-  for (int i = 0; i < 5; ++i) {
+  
+  for (int i = 0; i < 5; ++i)
+  {
     cin >> numbers[i];
   }
   
