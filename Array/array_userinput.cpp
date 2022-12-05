991d2e956857e30c2923b152964a52430778e5d6
HarshSingh21 <singhharsh8283@gmail.com>
2022-12-05 20:04:38 +0530

    user

---
+++ Array/array_userinput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int numbers[5];
+  cout << "Enter 5 numbers: " << endl;
+  for (int i = 0; i < 5; ++i) {
+    cin >> numbers[i];
+  }
+  cout << "The numbers are: ";
+  for (int n = 0; n < 5; ++n) {
+    cout << numbers[n] << "  ";
+  }
+  return 0;
+}
