fd2ee67013cf454c7460833d283dbddee50707dc
HarshSingh21 <singhharsh8283@gmail.com>
2023-01-28 18:05:37 +0530

    string

---
+++ String/strlen.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+     char *p;
+
+     cout<<"enter a string";
+     cin.getline(p,100);
+     cout<<"length"<<strlen(p)<<endl;
+    return 0;
+}
---
+++ String/vowelcheck.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int
+main ()
+{
+   string str = "how are felling harsh it is amazing";
+  int vowel=0,constant=0,space=0;
+   for(int i=0;str[i]!='\0';i++)
+   {
+      if(str[i]=='A' ||str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U' || str[i]=='a' || str[i]=='e'|| str[i]=='i' || str[i]=='o'|| str[i]=='u')
+      vowel++;
+      else if( str[i]==' ')
+      space++;
+      else
+      constant++;
+      
+      
+   }
+     
+cout<<"vowel is :"<<vowel<<endl;
+cout<<"space is :"<<space<<endl;
+cout<<"constant is:"<<constant;
+  return 0;
+}
