9836c39cbf70353eacb246dc8b28f7440f5761df
unknown <singhharsh8283@gmail.com>
2023-02-26 01:05:38 +0530

    while

---
+++ Pattern/while.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{     //while loop means check print increase 
+    int i = 1;
+    while(i<10)  //check 
+    {
+        cout<<"hello harsh"<<"\n"; //print 
+        i=i+1;//increase
+        
+    }
+    return 0;
+    
+}
