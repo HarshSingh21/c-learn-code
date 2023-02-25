4b48d93e0cdb2782aeead462f72b9806162250ea
unknown <singhharsh8283@gmail.com>
2023-02-26 00:48:10 +0530

    length

---
+++ String/len.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "harsh";
+    int len = s.size();
+    // cout<<(len);
+    s[len-1]='p';
+    cout<<s[len-1];
+    return 0;
+    
+}
