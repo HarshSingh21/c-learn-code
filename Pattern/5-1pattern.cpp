a368acf74b39b8279a8f608a06cdd22bb7e9e093
unknown <singhharsh8283@gmail.com>
2023-03-04 22:18:19 +0530

    update

--- Pattern/5-1pattern.cpp
+++ Pattern/5-1pattern.cpp
@@ -3,9 +3,9 @@ using namespace std;
     
 int main()
 { 
-	for(int i=0;i<5;i++)
+	for(int i=0;i<5;i++)  //row 
 	{
-	    for(int j=0;j<5;j++)
+	    for(int j=0;j<5;j++) //column
 	    {
 			if(i>j)
 		    cout<<" ";
