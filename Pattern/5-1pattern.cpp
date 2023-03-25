d30d150e442966029eecfcb794997ead0f94bcc8
unknown <singhharsh8283@gmail.com>
2023-03-25 18:16:35 +0530

    up

--- Pattern/5-1pattern.cpp
+++ Pattern/5-1pattern.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 { 
 	for(int i=0;i<5;i++) 
-	{
+	{   //row
 	    for(int j=0;j<5;j++) 
 	    {
 			if(i>j)
 		    cout<<" ";
 		    else
-		    cout<<"*";
+		    cout<<"*"; // column
 	    }
 	    cout<<endl;
 			} 
