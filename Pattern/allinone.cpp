03e22fe5954287250409b5ddca15cb81f2f92f66
Harsh <singhharsh8283@gmail.com>
2023-04-08 22:17:15 +0530

    update

--- Pattern/allinone.cpp
+++ Pattern/allinone.cpp
@@ -338,11 +338,9 @@ void print19(int n) {
           for(int j=1;j<=stars;j++){
               cout<<"*";
           }
-          
           for(int j = 1;j<=spaces;j++){
               cout<<" ";
           }
-          
           for(int j = 1;j<=stars;j++){
               cout<<"*";
           }
