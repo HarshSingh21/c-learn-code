02550f9a90470b8c4dad4fb812d24bb6b4ce1985
Harsh <singhharsh8283@gmail.com>
2023-04-09 01:01:37 +0530

    update

---
+++ basic/sqrt.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{     int a ;
+     cout<<"Enter to calculate square root "<<endl;
+     cin>>a;
+     cout<<sqrt(a);
+    return 0;
+}
