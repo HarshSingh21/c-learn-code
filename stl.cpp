e3f8178675e107abfa42ebda2bbc29bed41401b1
Harsh <singhharsh8283@gmail.com>
2023-04-09 02:48:59 +0530

    update

--- stl.cpp
+++ stl.cpp
@@ -32,3 +32,59 @@ int main()
 // cout <<"After  erase "<<l.size()<<endl;
 
 // }
+
+
+//DEQUE 
+
+
+// {
+
+
+// deque<int> d;
+// d.push_back(1);
+// d.push_front(2);
+
+// for(int i :d)
+// {
+//     cout<<i<<" ";
+ 
+// }  cout <<"\n";
+
+
+// cout<<d.size()<<endl;
+
+// cout<<"print index of element "<<d.at(1)<<endl;
+// cout<<"First element "<<d.front()<<endl;
+
+// cout<<"last element "<<d.back()<<endl;
+// cout<<"empty or not "<<d.empty()<<endl;
+
+// cout <<"Before erase "<<d.size()<<endl;
+// d.erase(d.begin(),d.begin()+1);
+// cout <<"After  erase "<<d.size()<<endl;
+
+// for(int i :d)
+// {
+//     cout<<i <<" ";
+// }
+// cout<<endl;
+
+
+
+
+// d.pop_back();
+// cout<<"POP from back ";
+// for(int i :d)
+// {
+//     cout<<i<<" ";
+ 
+// }  cout <<"\n";   // = 2 
+
+// //
+// d.pop_front();
+// cout<<"POP from front ";
+// for(int i :d)
+// {
+//     cout<<i<<" ";    // ==    1  
+ 
+// }  cout <<"\n";}
