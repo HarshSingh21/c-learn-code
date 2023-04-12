5806cada421ab06aed0bb634375239e2a8b0c8d9
Harsh <singhharsh8283@gmail.com>
2023-04-12 23:21:04 +0530

    rec

--- Recursion/intro.cpp
+++ Recursion/intro.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
 using namespace std ;
+// print name n time using recursion 
 
-void f(int i,int n)
-{
-  if(i>n) return ;
-  cout<<n<<endl;
 
-  f(i,n-1);
-}
+// void func(int i, int n){
+   
+//    // Base Condition.
+//    if(i>n) return;
+//    cout<<"Harsh"<<endl;
 
-int main()
-{
-  int n ;
-  cin>>n;
-  f(1,n);
-}
+//    // Function call to print till i increments.
+//    func(i+1,n);
+
+// }
+
+// int main(){
+  
+//   // Here, let’s take the value of n to be 4.
+//   int n ;
+//   cin>>n;
+//   func(1,n);
+//   return 0;
+// }*/
+
+
+
+
+
+
+
+
+// /
+// void f(int i,int n)
+// {
+//   if(i>n) return ;
+//   cout<<n<<endl;
+
+//   f(i,n-1);
+// }
+
+// int main()
+// {
+//   int n ;
+//   cin>>n;
+//   f(1,n);
+// }
+
