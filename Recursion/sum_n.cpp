498b47de8b330c2774f551bfcc5302aef27728dd
Harsh <singhharsh8283@gmail.com>
2023-04-12 23:46:19 +0530

    rec

--- Recursion/sum_n.cpp
+++ Recursion/sum_n.cpp
@@ -25,3 +25,33 @@ int main(){
   return 0;
 
 }
+
+
+
+// Functional way
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int func(int n){
+   
+   // Base Condition.
+   if(n == 0)
+   {
+       return 0;
+   }
+
+   // Problem broken down into 2 parts and then combined.
+   return n + func(n-1);
+
+}
+
+int main(){
+  
+  // Here, let’s take the value of n to be 3.
+  int n = 3;
+  cout<<func(n)<<endl;
+  return 0;
+
+}
