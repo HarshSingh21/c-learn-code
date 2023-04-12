6c05878d0c0ca3764642afb95214551903e9cfcb
Harsh <singhharsh8283@gmail.com>
2023-04-12 23:49:33 +0530

    rec

---
+++ Recursion/fact.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n){
+   
+   // Base Condition.
+   if(n == 0)
+   {
+       return 1;
+   }
+
+   // Problem broken down into 2 parts and then combined.
+   return n * factorial(n-1);
+
+}
+
+int main(){
+  
+  // Here, let’s take the value of n to be 3.
+  int n = 3;
+  cout<<factorial(n)<<endl;
+  return 0;
+
+}
