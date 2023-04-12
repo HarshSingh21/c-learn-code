07735a2cf36ed5eb42308b7f8d1ce239e32a4171
Harsh <singhharsh8283@gmail.com>
2023-04-12 23:22:42 +0530

    rec

--- Recursion/intro.cpp
+++ Recursion/intro.cpp
@@ -24,10 +24,33 @@ using namespace std ;
 // }*/
 
 
+// 1-n 
+// void func(int i, int n){
+   
+//    // Base Condition.
+//    if(i>n) return;
+//    cout<<i<<endl;
+
+//    // Function call to print i till i increments to n.
+//    func(i+1,n);
+
+// }
+
+// int main(){
+  
+//   // Here, let’s take the value of n to be 4.
+//   int n = 4;
+//   func(1,n);
+//   return 0;
+
+// }
 
 
 
 
+
+
+//   n-1 
 
 
 // /
