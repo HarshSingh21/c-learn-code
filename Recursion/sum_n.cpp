780775c0f7283b442ea938956c076391539ac4dd
Harsh <singhharsh8283@gmail.com>
2023-04-16 21:46:55 +0530

    update

--- Recursion/sum_n.cpp
+++ Recursion/sum_n.cpp
@@ -31,27 +31,29 @@ int main(){
 // Functional way
 
 
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int func(int n){
+// int func(int n){
    
-   // Base Condition.
-   if(n == 0)
-   {
-       return 0;
-   }
+//    // Base Condition.
+//    if(n == 0)
+//    {
+//        return 0;
+//    }
 
-   // Problem broken down into 2 parts and then combined.
-   return n + func(n-1);
+//    // Problem broken down into 2 parts and then combined.
+//    return n + func(n-1);
 
-}
+// }
 
-int main(){
+
+// function way 
+// int main(){
   
-  // Here, let’s take the value of n to be 3.
-  int n = 3;
-  cout<<func(n)<<endl;
-  return 0;
+//   // Here, let’s take the value of n to be 3.
+//   int n = 3;
+//   cout<<func(n)<<endl;
+//   return 0;
 
-}
+// }
