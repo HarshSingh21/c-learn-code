4a67a27d5d0035b01c19bd26cfcec321c48b376e
Harsh <singhharsh8283@gmail.com>
2023-06-12 10:57:18 +0530

    update

--- basic/count digit.cpp
+++ basic/count digit.cpp
@@ -15,3 +15,27 @@ int main() {
 
  return 0;
 }
+
+
+// //
+// #include <iostream>
+// using namespace std;
+
+// int  countfun(int x )
+// {
+//     int res =0;
+//     while(x>0)
+//     {
+//         x=x/10;
+//         res++;
+//     }
+//     return res;
+// }
+
+// int main()
+
+// {
+//  int result = countfun(565);
+//     cout << result;
+//     return 0;
+// }
